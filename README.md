5b93f264d237931b9ef772d576675cd72ed0f7af
Victor Siu <siuleungjing@gmail.com>
2023-03-13 14:31:12 -0700

    Fix bugs
    Fix favicon

--- README.md
+++ README.md
@@ -1,46 +1,25 @@
-# Getting Started with Create React App
+# Calculator App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a React-Typescript app created for the take-home assignment for Vial Health.
 
-## Available Scripts
+## How to Run
 
-In the project directory, you can run:
+1. Clone the repository.
+2. Run `npm install` to install all dependencies.
+3. Run `npm start` to start the app on your browser
 
-### `npm start`
+## Added Features
+- The calculator should have a browser-based user interface (ie it can be opened using a web browser)
+- The calculator should have a number pad with digits 0-9 and decimal point.
+- The calculator should have buttons for addition, subtraction, multiplication, and division.
+- The calculator should have a display that shows the input and the result of the calculation.
+- The calculator should follow the order of operations (PEMDAS).
+- Percentage function (%)
+- History function
+- Better UI
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Notes
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+There is a strange bug that crops up with the display in Chrome, specifically
+when your input the minus sign. I could not reproduce this bug on any other
+browser, leading me to believe that it is an issue with Chrome.
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,185 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button, Container, Grid, Paper, styled } from "@mui/material";
+import { GridDigitButton } from "./GridDigitButton";
+import { GridOperationButton } from "./GridOperationButton";
+
+const OutputContainer = styled(`div`)(({ theme }) => ({
+  width: "100%",
+  textAlign: "right",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize: "3em",
+  overflow: "hidden",
+}));
+
+const CalculatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius: 15,
+}));
 
 function App() {
+  const [prevValue, setPrevValue] = useState("0");
+  const [currentValue, setCurrentValue] = useState("0");
+  const [displayValue, setDisplayValue] = useState("0");
+  const [operation, setOperation] = useState("");
+  const [overwrite, setOverwrite] = useState(true);
+
+  const equals = () => {
+    var curr = 0;
+    try {
+      console.log(currentValue.replace("Ans", prevValue));
+      curr = eval(currentValue.replace("Ans", prevValue));
+    } catch (err) {
+      console.log(err);
+      setDisplayValue("Error");
+      setCurrentValue("0");
+      setPrevValue("0");
+      setOverwrite(true);
+      return;
+    }
+    setCurrentValue(`${curr}`);
+    setDisplayValue(parseDisplay(`${curr}`));
+    setPrevValue(`${curr}`);
+    setOperation("");
+    setOverwrite(true);
+  };
+
+  const parseDisplay = (x: string) => {
+    var num = Number(parseFloat(x));
+    console.log(x);
+    var truncatedNum = num.toPrecision(Math.min(num.toString().length, 13));
+    if (truncatedNum.includes(".")) {
+      if (truncatedNum.includes("e")) {
+        var index = truncatedNum.lastIndexOf("e");
+        var power = truncatedNum.substring(index);
+        truncatedNum = truncatedNum.substring(0, index).replace(/(\.\d*?[1-9])0+$/g, "$1" ) + power;
+      } else {
+        truncatedNum = truncatedNum.replace(/(\.\d*?[1-9])0+$/g, "$1" );
+      }
+    }
+    if (truncatedNum.endsWith(".0")) {
+      return truncatedNum.substring(0, truncatedNum.length - 2);
+    }
+    return truncatedNum;
+  }
+
+  const clear = () => {
+    setPrevValue("0");
+    setOperation("");
+    setCurrentValue("0");
+    setDisplayValue("0");
+    setOverwrite(true);
+  };
+
+  const del = () => {
+    setCurrentValue("0");
+    setDisplayValue("0");
+    setOverwrite(true);
+  };
+
+  const percent = () => {
+    const curr = parseFloat(currentValue);
+    setCurrentValue((curr / 100).toString());
+    setDisplayValue(parseDisplay((curr / 100).toString()));
+  };
+
+  const selectOperation = (op: string) => {
+    setCurrentValue(`${currentValue}${op}`);
+    setDisplayValue(`${currentValue}${op}`);
+    console.log(currentValue, displayValue);
+    setOverwrite(false);
+  };
+
+  const setDigit = (digit: string) => {
+    if (overwrite && digit !== ".") {
+      setCurrentValue(digit);
+      setDisplayValue(digit);
+    } else {
+      console.log(currentValue, displayValue);
+      setCurrentValue(`${currentValue}${digit}`);
+      setDisplayValue(`${currentValue}${digit}`);
+    }
+    setOverwrite(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <CalculatorBase elevation={3}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <OutputContainer data-testid="output">
+              <div style={{float:"right"}}>
+                {displayValue}
+              </div>
+            </OutputContainer>
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridOperationButton
+              operation={"AC"}
+              selectOperation={clear}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation={"C"}
+              selectOperation={del}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation={"%"}
+              selectOperation={percent}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation={"/"}
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit={"7"} enterDigit={setDigit} />
+            <GridDigitButton digit={"8"} enterDigit={setDigit} />
+            <GridDigitButton digit={"9"} enterDigit={setDigit} />
+            <GridOperationButton
+              operation={"*"}
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit={"4"} enterDigit={setDigit} />
+            <GridDigitButton digit={"5"} enterDigit={setDigit} />
+            <GridDigitButton digit={"6"} enterDigit={setDigit} />
+            <GridOperationButton
+              operation={"-"}
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit={"1"} enterDigit={setDigit} />
+            <GridDigitButton digit={"2"} enterDigit={setDigit} />
+            <GridDigitButton digit={"3"} enterDigit={setDigit} />
+
+            <GridOperationButton
+              operation={"+"}
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit={"Ans"} enterDigit={setDigit} />
+            <GridDigitButton digit={"0"} enterDigit={setDigit} />
+            <GridDigitButton digit={"."} enterDigit={setDigit} />
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained" onClick={equals}>
+                =
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CalculatorBase>
+    </Container>
   );
 }
 
